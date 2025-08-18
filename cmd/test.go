32aa73ba50dd7561371e32257a19419c9d7b96de
amingma <jiama19999@gmail.com>
2025-08-18 15:01:49 -0500

    integrated graph and tree

--- cmd/test.go
+++ cmd/test.go
@@ -4,6 +4,8 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
 	"os/exec"
 	"strings"
@@ -27,25 +29,65 @@ var testCmd = &cobra.Command{
 			cmd.Println("Only c++ files supported soz")
 			os.Exit(1)
 		}
+		problemType, _ := cmd.Flags().GetString("type")
+		if problemType != "array" && problemType != "graph" && problemType != "tree" {
+			cmd.Println("Only 'array', 'graph', and 'tree' options currently supported")
+			os.Exit(1)
+		}
 		compile(args[0])
 		compile(args[1])
-		numCases, _ := cmd.Flags().GetInt("test")
+		numCases, _ := cmd.Flags().GetInt("cases")
+		multiQuery, _ := cmd.Flags().GetInt("multi-query")
+		var pass bool = true
 		for range numCases {
-			arr := gen.GenerateRandomArray(3, 1, 6)
-			testCmd := exec.Command("./main")
-			testCmd.Stdin = strings.NewReader(arr)
-			output, err := testCmd.Output()
-			if err == nil {
-				cmd.Println(string(output))
+			var input string
+			if problemType == "array" {
+				num := rand.Intn(10) + 1
+				input = gen.GenerateRandomArray(num, 1, 1e9)
+			} else if problemType == "tree" {
+				num := rand.Intn(10) + 1
+				input = gen.GenerateRandomTree(num)
+			} else if problemType == "graph" {
+				num := rand.Intn(10) + 1
+				num_edges := rand.Intn(num*(num-1)/2-num+2) + num - 1
+				input = gen.GenerateRandomGraph(num, num_edges)
 			}
+			if multiQuery == 1 {
+				input = "1\n" + input
+			}
+			var executable strings.Builder
+			fmt.Fprintf(&executable, "./%s", args[0][:len(args[0])-4])
+			testCmd := exec.Command(executable.String())
+			testCmd.Stdin = strings.NewReader(input)
+			output1, err1 := testCmd.Output()
+			executable.Reset()
+			fmt.Fprintf(&executable, "./%s", args[1][:len(args[1])-4])
+			testCmd = exec.Command(executable.String())
+			testCmd.Stdin = strings.NewReader(input)
+			output2, err2 := testCmd.Output()
+			if err1 == nil && err2 == nil {
+				if string(output1) != string(output2) {
+					pass = false
+					cmd.Println("Outputs differed on following test case: ")
+					cmd.Print(input)
+					cmd.Printf("\nUser output: %s", string(output1))
+					cmd.Printf("Solution output: %s", string(output2))
+					os.Exit(1)
+				}
+			}
+		}
+		if pass {
+			cmd.Println("All cases passed!")
+			os.Exit(0)
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(testCmd)
-	testCmd.Flags().IntP("test", "t", 1, "number of test cases")
-	// testCmd.MarkFlagRequired("test")
+	testCmd.Flags().IntP("cases", "n", 1, "number of test cases")
+	testCmd.Flags().StringP("type", "t", "array", "problem input category")
+	testCmd.Flags().IntP("multi-query", "q", 1, "multiple queries in single test case (1/0)")
 }
 
 func verifyFile(filePath string) bool {
